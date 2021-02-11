8fc391f93e57e20539aede2809bbf05e26a1b08d
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-11 21:17:27 +0100

    artwork 2,3,4

--- art.js
+++ art.js
@@ -10,3 +10,28 @@ for(let i=100;i<300;i=i+20) {
     //append
     art1.appendChild(box);
 }
+
+const art2 = document.getElementById("artwork2");
+for(let i=0;i<90;i=i+10) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    box.style.transform = "rotate("+ i +"deg)" ;
+    art2.appendChild(box); 
+}
+
+const art3 = document.getElementById("artwork3");
+for(let i=0;i<200;i=i+10) {
+    let circle = document.createElement("div");
+    circle.classList.add("circle");
+    circle.style.width = i + "px";
+    circle.style.height = i + "px";
+    art3.appendChild(circle);
+}
+
+const art4 = document.getElementById("artwork4");
+for(i=-90;i<90;i=i+20) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    box.style.transform = "translate(" + i + "px, " + i + "px)";
+    art4.appendChild(box);
+}
